52e946b5a6b860518d0c9c978157c806ef30eaab
stellatsimi <83275101+stellatsimi@users.noreply.github.com>
2022-11-29 20:19:17 +0200

    Update createDB.sql

--- SQL scripts/createDB.sql
+++ SQL scripts/createDB.sql
@@ -70,3 +70,80 @@ CREATE TABLE IF NOT EXISTS manages (
     CONSTRAINT mng_fk_adm FOREIGN KEY (mng_adm_AT) REFERENCES admin(adm_AT) ON DELETE CASCADE ON UPDATE CASCADE,
     CONSTRAINT mng_fk_br FOREIGN KEY (mng_br_code) REFERENCES branch(br_code) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS event (
+    ev_tr_id INT(11) AUTO_INCREMENT NOT NULL,
+    ev_start TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP (),
+    ev_end TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP (),
+    ev_descr TEXT DEFAULT 'UNKNOWN',
+    PRIMARY KEY (ev_tr_id , ev_start)
+);
+
+CREATE TABLE IF NOT EXISTS trip (
+    tr_id INT(11) NOT NULL,
+    tr_departure TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP (),
+    tr_return TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP (),
+    tr_maxseats TINYINT(4) NOT NULL,
+    tr_cost FLOAT(7 , 2 ) NOT NULL,
+    tr_br_code INT(11) NOT NULL,
+    tr_gui_code INT(11) NOT NULL,
+    tr_gui_AT CHAR(10) NOT NULL,
+    tr_drv_AT CHAR(10) NOT NULL,
+    PRIMARY KEY (tr_id),
+    CONSTRAINT tr_id_ FOREIGN KEY (tr_id)
+        REFERENCES event (ev_tr_id)
+        ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT tr_br_code_ FOREIGN KEY (tr_br_code)
+        REFERENCES branch (br_code)
+        ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT tr_drv_AT_ FOREIGN KEY (tr_drv_AT)
+        REFERENCES driver (drv_AT)
+        ON DELETE CASCADE ON UPDATE CASCADE
+);
+      
+CREATE TABLE IF NOT EXISTS languages (
+    lng_gui_AT CHAR(10) NOT NULL,
+    lng_language VARCHAR(30) NOT NULL,
+    PRIMARY KEY (lng_gui_AT),
+    CONSTRAINT lng_gui_AT_ FOREIGN KEY (lng_gui_AT)
+        REFERENCES guide (gui_AT)
+        ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS travel_to (
+    to_tr_id INT(11) NOT NULL,
+    to_dst_id INT(11) NOT NULL,
+    to_arrival TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP (),
+    to_departure TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP (),
+    PRIMARY KEY (to_tr_id , to_dst_id),
+    CONSTRAINT to_tr_id_ FOREIGN KEY (to_tr_id)
+        REFERENCES trip (tr_id)
+        ON DELETE CASCADE ON UPDATE CASCADE
+);
+CREATE TABLE IF NOT EXISTS destination (
+    dst_id INT(11) NOT NULL,
+    dst_name VARCHAR(50) NOT NULL DEFAULT 'UNKWOWN',
+    dst_descr TEXT,
+    dsrt_type ENUM('LOCAL', 'ABROAD'),
+    dst_language VARCHAR(30) NOT NULL,
+    dst_location INT(11) NOT NULL,
+    PRIMARY KEY (dst_id),
+    CONSTRAINT dst_id_ FOREIGN KEY (dst_id)
+        REFERENCES travel_to (to_dst_id)
+        ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT dst_location_ FOREIGN KEY (dst_id)
+        REFERENCES destination (dst_id)
+        ON DELETE CASCADE ON UPDATE CASCADE
+);
+         
+CREATE TABLE IF NOT EXISTS reservation (
+    res_tr_id INT(11) NOT NULL,
+    res_seatnum TINYINT(4) NOT NULL,
+    res_name VARCHAR(20) NOT NULL DEFAULT 'UNKNOWN',
+    res_lname VARCHAR(20) NOT NULL DEFAULT 'UNKNOWN',
+    res_isadult ENUM('ADULT', 'MINOR'),
+    PRIMARY KEY (res_tr_id , res_seatnum),
+    CONSTRAINT res_tr_id_ FOREIGN KEY (res_tr_id)
+        REFERENCES trip (tr_id)
+        ON DELETE CASCADE ON UPDATE CASCADE
+);
