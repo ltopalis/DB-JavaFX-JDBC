8de46955c70a8780ea25466fc0aed37ffeec8118
Lazaros Topalis <83336644+ltopalis@users.noreply.github.com>
2022-11-29 18:51:36 +0200

    insert some data

---
+++ SQL scripts/insertData.sql
@@ -0,0 +1,31 @@
+USE travel_agency;
+
+-- branch table
+INSERT INTO branch(br_street, br_num, br_city) VALUES ('Πεσμαζόγλου', 5, 'Αθήνα');
+INSERT INTO branch(br_street, br_num, br_city) VALUES ('Αγίου Κωνσταντίνου', 22, 'Αθήνα');
+INSERT INTO branch(br_street, br_city) VALUES ('C. Ramos Marín', 'Málaga');
+INSERT INTO branch(br_street, br_num, br_city) VALUES ('Εθνικής Αμύνης', 2, 'Θεσσαλονίκη');
+INSERT INTO branch(br_street, br_num, br_city) VALUES ('Βασιλίσσης Όλγας', 150, 'Θεσσαλονίκη');
+INSERT INTO branch(br_street, br_num, br_city) VALUES ('Bahnhofstraße', 20, 'Münster');
+INSERT INTO branch(br_street, br_num, br_city) VALUES ('Glockengasse', 11, 'Köln');
+INSERT INTO branch(br_street, br_num, br_city) VALUES ('Fidicinstraße', 40, 'Berlin');
+INSERT INTO branch(br_street, br_num, br_city) VALUES ('Rue de la Gaité', 14, 'Paris');
+INSERT INTO branch(br_street, br_num, br_city) VALUES ('Rue Delille', 19, 'Nice');
+
+-- phones
+INSERT INTO phones VALUES (1, '2104595120');
+INSERT INTO phones VALUES (1, '6945399742');
+INSERT INTO phones VALUES (2, '2101257976');
+INSERT INTO phones VALUES (3, '9521558238');
+INSERT INTO phones VALUES (4, '2310744561');
+INSERT INTO phones VALUES (4, '2310481257');
+INSERT INTO phones VALUES (4, '6942154732');
+INSERT INTO phones VALUES (5, '2310893135');
+INSERT INTO phones VALUES (5, '2310574566');
+INSERT INTO phones VALUES (6, '2512046846');
+INSERT INTO phones VALUES (6, '1761548588');
+INSERT INTO phones VALUES (7, '1738415648');
+INSERT INTO phones VALUES (8, '1805448921');
+INSERT INTO phones VALUES (8, '3015644564');
+INSERT INTO phones VALUES (9, '1864154156');
+INSERT INTO phones VALUES (10, '8929090821');
