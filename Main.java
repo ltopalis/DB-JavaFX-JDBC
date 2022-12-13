7909216752beb204352f406a1012fa69f191c43f
Lazaros Topalis <83336644+ltopalis@users.noreply.github.com>
2022-12-13 20:04:12 +0200

    login scene

--- Main.java
+++ Main.java
@@ -1,49 +1,31 @@
+// colors: https://colorhunt.co/palette/ffb3b3ffdba4ffe9aec1efff
+
 import javafx.application.*;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.*;
 import javafx.scene.*;
-import javafx.scene.layout.*;
-import javafx.scene.control.*;
-import javafx.geometry.*;
 
-import java.time.LocalDate;
+import java.io.IOException;
 
 public class Main extends Application {
-    Button btn;
-    DatePicker dp;
 
     @Override
     public void start(Stage stage) {
-        btn = new Button();
-        btn.setText("Print Date");
-        btn.setOnAction(e -> {
-            LocalDate dt = dp.getValue();
-            if (dt!=null) {
-                System.out.println("Date: " + dt);
-                System.out.println("Day: " + dt.getDayOfMonth());
-                System.out.println("Month: " + dt.getMonth());
-                System.out.println("Year: " + dt.getYear());
-            }
-            else {
-                System.out.println("Choose a date!");
-            }
-        });
 
-        dp = new DatePicker();
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
 
-        HBox pane = new HBox();
-        pane.getChildren().addAll(dp, btn);
-        pane.setAlignment(Pos.CENTER);
-        pane.setSpacing(10);
+            Scene scene = new Scene(root, 1500, 900);
 
-        Scene scene = new Scene(pane, 300, 200);
+            stage.setScene(scene);
+            stage.setResizable(false);
+            //stage.initStyle(StageStyle.UNDECORATED);
+            stage.show();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
-
-        stage.setScene(scene);
-        stage.setTitle("DATE PICKER");
-
-        stage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
