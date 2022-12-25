6f620ffe435bab95031ba9d21de414ac5ff9d8d5
Lazaros Topalis <lazarostop32@gmail.com>
2022-12-25 17:11:25 +0200

    connect dashboard to login button

--- loginControllers.java
+++ loginControllers.java
@@ -1,11 +1,22 @@
+import java.io.IOException;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class loginControllers implements Initializable {
     @FXML
@@ -25,6 +36,8 @@ public class loginControllers implements Initializable {
 
     @FXML
     private Label notificationLabel;
+    private double x;
+    private double y;
 
     public void exitButtonClicked(ActionEvent e) {
         System.exit(0);
@@ -35,8 +48,61 @@ public class loginControllers implements Initializable {
         String password = passwordTF.getText();
         String result = connectDB.getAccess(username, password);
         if (result != null) {
-            notificationLabel.setText(result);
-            notificationLabel.setVisible(true);
+            String query = "SELECT w.wrk_lname, w.wrk_name, w.wrk_salary, w.wrk_br_code, it.password, it.start_date " +
+                    "FROM worker w JOIN it " +
+                        "ON w.wrk_AT=it.IT_AT " +
+                    "WHERE it.IT_AT = ?";
+
+            try(Connection conn = connectDB.getConnection()){
+                PreparedStatement stmt = conn.prepareStatement(query);
+                stmt.setString(1, result);
+                ResultSet info = stmt.executeQuery();
+                if(info.next()){
+                    userInformation.setAT(result);
+                    userInformation.setLastname(info.getString("wrk_lname"));
+                    userInformation.setName(info.getString("wrk_name"));
+                    userInformation.setSalary(info.getFloat("wrk_salary"));
+                    userInformation.setBranch(info.getInt("wrk_br_code"));
+                    userInformation.setPassword(info.getString("password"));
+                    userInformation.setStart_date(info.getDate("start_date").toLocalDate());
+                    notificationLabel.setText("Επιτυχής σύνδεση!");
+
+                    try{
+                        Parent root = FXMLLoader.load(getClass().getResource("fxml code/mainWindow.fxml"));
+                        Stage stage = new Stage();
+                        Scene scene = new Scene(root);
+
+                        root.setOnMousePressed((MouseEvent e) -> {
+                            x = e.getSceneX();
+                            y = e.getSceneY();
+                        });
+
+                        root.setOnMouseDragged((MouseEvent e) -> {
+                            stage.setX(e.getScreenX() - x);
+                            stage.setY(e.getSceneY() - y);
+
+                            stage.setOpacity(.8);
+                        });
+
+                        root.setOnMouseReleased((MouseEvent e) -> {
+                            stage.setOpacity(1);
+                        });
+
+                        stage.initStyle(StageStyle.TRANSPARENT);
+
+                        stage.setScene(scene);
+                        loginBtn.getScene().getWindow().hide();
+                        stage.show();
+                    }catch(IOException e){
+                        e.printStackTrace();
+                    }
+                }else
+                    notificationLabel.setText("Προέκυψε πρόβλημα. Ξαναπροσπαθείστε!");
+                notificationLabel.setVisible(true);
+
+            }catch(SQLException e){
+                e.printStackTrace();
+            }
         } else {
             notificationLabel.setText("Ο χρήστης δεν υπάρχει, δεν έχει πρόσβαση ή τα στοιχεία είναι λάθος!");
             notificationLabel.setVisible(true);
