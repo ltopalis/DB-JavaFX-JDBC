bb74a4b8bef2cde2284e4ba5524bfc43b52ca549
Lazaros Topalis <lazarostop32@gmail.com>
2022-12-28 14:43:32 +0200

    add batch inserts

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import math
 import random
 
-records = math.floor(6e4)
+records = math.floor(90_000)
 number_of_offers = 3
 
 if records % number_of_offers != 0:
@@ -20,9 +20,21 @@ with open("names.txt", "r") as f:
     for i in range(number_of_offers):
         mydict[str(i + 1)] = records // number_of_offers
 
-    with open("SQL scripts\insertData.sql", "a") as f2:
+    with open("SQL scripts/insertData.sql", "a") as f2:
         f2.write("\n\n-- reservation_offers table\n")
-        for i in range(records):
+        randomName = random.sample(name, k=1)[0]
+        randomSurname = random.sample(surname, k=1)[0]
+
+        randomTrip = random.randint(1, number_of_offers)
+        while mydict[str(randomTrip)] == 0:
+            randomTrip = random.randint(1, number_of_offers)
+        else:
+            mydict[str(randomTrip)] -= 1
+
+        randomDeposit = round(random.uniform(50, 200), 2)
+        sqlQuery = f'INSERT INTO reservation_offers VALUES (NULL, "{randomSurname}", "{randomName}", {randomTrip}, {randomDeposit})'
+        f2.write(sqlQuery)
+        for i in range(records-1):
             randomName = random.sample(name, k=1)[0]
             randomSurname = random.sample(surname, k=1)[0]
 
@@ -33,7 +45,8 @@ with open("names.txt", "r") as f:
                 mydict[str(randomTrip)] -= 1
 
             randomDeposit = round(random.uniform(50, 200), 2)
-            sqlQuery = f'INSERT INTO reservation_offers VALUES (NULL, "{randomSurname}", "{randomName}", {randomTrip}, {randomDeposit});\n'
+            sqlQuery = f',\n\t\t\t\t\t\t\t\t\t  (NULL, "{randomSurname}", "{randomName}", {randomTrip}, {randomDeposit})'
             f2.write(sqlQuery)
+        f2.write(";\n")
 
-print("finished")
+print(f"finished ({i+1+1})")
